3376cec1ebaae61de5efa53cee73a37cc8a33303
Oleg <olegzendel@campus.technion.ac.il>
2018-01-05 19:38:21 +0200

    Total 4 bugs in Buggy Code
    All details in the logfile

--- Buggy Solution/Interface.cpp
+++ Buggy Solution/Interface.cpp
@@ -15,69 +15,59 @@ using std::string;
 
 #define TANK_FULL (5) // define threshold for full the cleaning tank
 
-void StartControl()
-{
+void StartControl() {
     string command, name; // command- the command from the user, name- robot's name from the user
     string dir; // The directions that the robot needs to move in the map, input from the user
-    int x,y; // coordinates for location in the map, input from the user
+    int x, y; // coordinates for location in the map, input from the user
 
     std::cout << "Start entering commands:" << std::endl;
 
-    while (cin >> command){
-       if (command=="Place"){
-          cin>>name>>x>>y;
-          if ( inMapLimit(x,y) && place_robot(name,x,y)  )
-          {
-            PrintRobotPlace(name, x, y);
-          }
-       }
-       else if (command=="Delete"){
-    	   cin>>name;
-    	   delete_robot(name);
-       }
-       else if (command=="Move"){
-    	   cin>>name>>dir;
-    	   move(name,dir);
-       }
-       else if (command=="AddDirt"){
-    	   cin>>x>>y;
-    	   if (inMapLimit(x,y))
-			   if (!is_location_robot(x,y))
-				   addDirt(x,y);
-       }
-       else if (command=="Clean"){
-    	   cin>>name;
-    	   const int index = find_robot(name);
-    	   if (index != -1){
-        	   int x, y;
-        	   get_cord(index, x, y);
-        	   if (getTankCounter(index) < TANK_FULL){
-        		   if (x!=-1 && y!=-1){
-            		   bool dirty = isDirty(x,y);
-            		   clean(x,y);
-            		   updateTank(index);
-            		   if (dirty)
-            			   updateRank(index);
-            		   PrintClean(name, x, y);
-        		   }
-        	   }
-        	   else emptyTank(name, index);
-    	   }
+    while (cin >> command) {
+        if (command == "Place") {
+            cin >> name >> x >> y;
+            if (inMapLimit(x, y) && place_robot(name, x, y)) {
+                PrintRobotPlace(name, x, y);
+            }
+        } else if (command == "Delete") {
+            cin >> name;
+            delete_robot(name);
+        } else if (command == "Move") {
+            cin >> name >> dir;
+            move(name, dir);
+        } else if (command == "AddDirt") {
+            cin >> x >> y;
+            if (inMapLimit(x, y))
+                if (!is_location_robot(x, y))
+                    addDirt(x, y);
+        } else if (command == "Clean") {
+            cin >> name;
+            const int index = find_robot(name);
+            if (index != -1) {
+                get_cord(index, x, y);
+                if (getTankCounter(index) <= TANK_FULL) {
+                    if (x != -1 && y != -1) {
+                        bool dirty = isDirty(x, y);
+                        clean(x, y);
+                        updateTank(index);
+                        if (dirty)
+                            updateRank(index);
+                        PrintClean(name, x, y);
+                    }
+                } else emptyTank(name, index);
+            }
 
-       }
-		else if (command=="MoveMulti"){
-			cin>>name;
-			while (cin>>dir){
-				if(dir=="end")
-					break;
-				move(name,dir);
-			}
-		}
-		else std::cout << "Please enter a valid command" << std::endl;
-  }
+        } else if (command == "MoveMulti") {
+            cin >> name;
+            cin >> dir;
+            do {
+                move(name, dir);
+                cin >> dir;
+            } while (dir != "end");
+        } else std::cout << "Please enter a valid command" << std::endl;
+
+        /* just for debugging */
+//        PrintMap();
+//        PrintTable(getRobots(), getRanks(), getTanks());
+    }
     PrintTable(getRobots(), getRanks(), getTanks());
-    //PrintMap(); // just for debugging
 }
-
-
-
